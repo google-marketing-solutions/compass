4c956fc20781c97d30f1c292070627263f14a565
Azamat Beknazarov <azamat@google.com>
2022-02-13 16:45:45 -0800

    Add support for regression model.
    PiperOrigin-RevId: 428386262
    Change-Id: I8f5b5ed65c8753f598711f6550d5aab440af5830

--- packages/utils/model.py
+++ packages/utils/model.py
@@ -11,8 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Propensity model templated module(s) based on BigQeury ML."""
+"""Classification and Regression model templated module(s) based on BigQeury ML."""
 
+import abc
 import enum
 import logging
 import sys
@@ -27,20 +28,63 @@ logging.basicConfig(
     format='%(levelname)s: %(message)s', level=logging.INFO, stream=sys.stdout)
 
 _TEMPLATES_DIR = 'templates'
-_CLOUD_CONSOLE_URL = 'https://console.cloud.google.com/bigquery?project='
 _ParamsType = Mapping[str, Union[str, List[str], Mapping[str, str]]]
 
 
 class ModelTypes(enum.Enum):
+  """Supported model types."""
+  REGRESSION = 'REGRESSION'
+  CLASSIFICATION = 'CLASSIFICATION'
+
+  def validate_model_type(self, model_type: str) -> None:
+    """Validates supported model types.
+
+    Args:
+      model_type: Type of the model.
+
+    Raises:
+      ValueError: If model type is not supported.
+    """
+    if self == ModelTypes.REGRESSION:
+      model_types = RegressionModelTypes
+    elif self == ModelTypes.CLASSIFICATION:
+      model_types = ClassificationModelTypes
+    else:
+      raise ValueError(f'{model_type} is not supported.')
+
+    supported_model_types = model_types.supported_types()
+    if model_type.upper() not in supported_model_types:
+      raise ValueError(
+          f'"{model_type}" is not supported. Use one of {supported_model_types}.'
+      )
+
+
+class ClassificationModelTypes(enum.Enum):
   """Supported model types for PropensityModel."""
   LOGISTIC_REG = 'LOGISTIC_REG'
   AUTOML_CLASSIFIER = 'AUTOML_CLASSIFIER'
   BOOSTED_TREE_CLASSIFIER = 'BOOSTED_TREE_CLASSIFIER'
   DNN_CLASSIFIER = 'DNN_CLASSIFIER'
 
+  @staticmethod
+  def supported_types() -> List[str]:
+    return [model.value for model in ClassificationModelTypes]
 
-class PropensityModel:
-  """Interacts with BigQuery ML to create and evaluate propensity models."""
+
+class RegressionModelTypes(enum.Enum):
+  """Supported model types for LTVModel."""
+  LINEAR_REG = 'LINEAR_REG'
+  AUTOML_REGRESSOR = 'AUTOML_REGRESSOR'
+  BOOSTED_TREE_REGRESSOR = 'BOOSTED_TREE_REGRESSOR'
+  DNN_REGRESSOR = 'DNN_REGRESSOR'
+
+  @staticmethod
+  def supported_types() -> List[str]:
+    return [model.value for model in RegressionModelTypes]
+
+
+class Model(abc.ABC):
+  """This is an abstract class for Compass Package models."""
 
   def __init__(self, bq_utils: bigquery_utils.BigQueryUtils,
                params: _ParamsType):
@@ -63,50 +107,62 @@ class PropensityModel:
       raise ValueError('No target column specified. Provide correct target '
                        'column to train the model.')
 
-  def _validate_model_type(self, model_type: str) -> None:
-    """Validates propensity model options.
+  def _validate_training_params(self, model_type: ModelTypes) -> None:
+    """Validates all required parameters for initializing BQML model.
 
     Args:
-      model_type: BigQuery ML model type as defined here
-      https://cloud.google.com/bigquery-ml/docs/reference/standard-sql/bigqueryml-syntax-create
-
-    Raises:
-      ValueError: If model type is not supported.
-    """
-    supported_model_types = [model.value for model in ModelTypes]
-    if model_type.upper() not in supported_model_types:
-      raise ValueError(
-          f'"{model_type}" is not supported. Use one of {supported_model_types}.'
-      )
-
-  def _validate_training_params(self, params: _ParamsType) -> None:
-    """Validates all required parameters for initializing PropensityModel.
-
-    Args:
-      params: GCP and BQML model option configuration parameters.
+      model_type: Type of the model.
     """
     # TODO(): Add validators for other BQML model options.
-    for column in [params['feature_columns'], params['target_column']]:
+    for column in [
+        self.params['feature_columns'], self.params['target_column']
+    ]:
       self._validate_columns(column)
-    self._validate_model_type(params['model_type'])
+    model_type.validate_model_type(str(self.params['model_type']))
 
   def _display_url(self, url_part: str, text: str = 'Access URL.') -> None:
     """Displays newly created BigQuery Dataset, Table or Model URLs.
 
     Args:
-      url_part: Newly generated URL part concatenated to _CLOUD_CONSOLE_URL.
+      url_part: Newly generated URL part concatenated to cloud console url.
       text: URL text to show to user.
     """
-    # Make sure to write html tag only in Jupyter notebook environment.
-    url = f'{_CLOUD_CONSOLE_URL}{url_part}'
+    cloud_console_url = 'https://console.cloud.google.com/bigquery?project='
+    # Makes sure to write html tag only in Jupyter notebook environment.
+    url = f'{cloud_console_url}{url_part}'
     if 'ipykernel' in sys.modules:
       html_tag = f'<a href={url}>{text}</a>'
       display.display(display.HTML(html_tag))
     else:
       logging.info(url)
 
+  def _display_model_url(self) -> None:
+    project_id, dataset, model_name = str(self.params['model_path']).split('.')
+    url = (f'{project_id}&ws=&p={project_id}&d={dataset}'
+           f'&m={model_name}&page=model')
+
+    logging.info('Finished training. Model can be access via following URL:')
+    self._display_url(url, 'BigQuery Model.')
+
+  def _display_table_url(self,
+                         table_path: str,
+                         text: str = 'BigQuery Table.') -> None:
+    project_id, dataset, table = table_path.split('.')
+    url = f'&d={dataset}&p={project_id}&t={table}&page=table'
+
+    self._display_url(url, text)
+
+  def _get_train_params(self) -> _ParamsType:
+    """Extracts train params to be used in sql template.
+
+    Returns:
+      _ParamsType with flattened key value pair.
+    """
+    return {key: value for key, value in self.params.items()}
+
+  @abc.abstractmethod
   def train(self, verbose: bool = False) -> pd.DataFrame:
-    """Trains propensity model in BigQuery ML.
+    """Trains BigQuery ML.
 
     Args:
       verbose: If set true, prints parsed SQL content.
@@ -114,22 +170,28 @@ class PropensityModel:
     Returns:
       Training BigQuery job results as Pandas DataFrame.
     """
-    self._validate_training_params(self.params)
-    # Flatten nested dict params for parser to apply params to sql template.
-    train_params = {key: value for key, value in self.params.items()}
-    sql = utils.render_jinja_sql(
-        template_dir=_TEMPLATES_DIR,
-        template_name='classification',
-        verbose=verbose,
-        **train_params)
-    training_job = self.bq_utils.run_query(sql)
 
-    project_id, dataset, model_name = str(self.params['model_path']).split('.')
-    url = (f'{project_id}&ws=&p={project_id}'
-           f'&d={dataset}&m={model_name}&page=model')
-    logging.info('Finished training. Model can be access via following URL:')
-    self._display_url(url, 'BigQuery Model.')
-    return training_job.to_dataframe()
+  @abc.abstractmethod
+  def evaluate(self, verbose: bool = False) -> List[pd.DataFrame]:
+    """Evaluates pretrained BigQuery ML model.
+
+    Args:
+      verbose: If set true, prints parsed SQL content.
+
+    Returns:
+      List of DataFrames containing model's evaluation metrics.
+    """
+
+  @abc.abstractmethod
+  def predict(self, verbose: bool = False) -> pd.DataFrame:
+    """Predicts based on pretrained BigQuery ML model.
+
+    Args:
+      verbose: If set true, prints parsed SQL content.
+
+    Returns:
+      Prediction BigQuery job results as Pandas DataFrame.
+    """
 
   def get_feature_info(self,
                        model_path: Optional[str] = None,
@@ -137,9 +199,7 @@ class PropensityModel:
     """Retreives dataset feature information from BigQuery training dataset.
 
     Args:
-      model_path: Full path to BigQuery model. Ex: project.dataset.model. This
-        is optional, if not provided, default model path specified in params is
-        used to extract feature inforamation.
+      model_path: Full path to BigQuery model. Ex: project.dataset.model.
       verbose: If set true, prints parsed SQL content.
 
     Returns:
@@ -156,6 +216,107 @@ class PropensityModel:
         verbose=verbose,
         **params)
     return self.bq_utils.run_query(sql).to_dataframe()
+
+
+class LTVModel(Model):
+  """Interacts with BigQuery ML to create and evaluate LTV models."""
+
+  def train(self, verbose: bool = False) -> None:
+    """Trains regression model in BigQuery ML.
+
+    Args:
+      verbose: If set true, prints parsed SQL content.
+
+    Returns:
+      Training BigQuery job results as Pandas DataFrame.
+    """
+    self._validate_training_params(model_type=ModelTypes.REGRESSION)
+    train_params = self._get_train_params()
+    sql = utils.render_jinja_sql(
+        template_dir=_TEMPLATES_DIR,
+        template_name='regression',
+        verbose=verbose,
+        **train_params)
+    self.bq_utils.run_query(sql)
+    self._display_model_url()
+
+  def evaluate(self,
+               params: Optional[Mapping[str, Union[str, float]]] = None,
+               verbose: bool = False) -> pd.DataFrame:
+    """Evaluates BigQuery ML trained regression model.
+
+    Args:
+      params: Additional evaluation parameters containing evaluation dataset
+        name and threshold value.
+      verbose: If set true, prints parsed SQL content.
+
+    Returns:
+      DataFrame containing model's mean_absolute_error, mean_squared_error,
+      mean_squared_log_error, median_absolute_error, r2_score and
+      explained_variance metrics.
+    """
+    eval_params = {'model_path': self.params['model_path']}
+    if eval_params:
+      eval_params.update(params)
+
+    sql = utils.render_jinja_sql(
+        template_dir=_TEMPLATES_DIR,
+        template_name='regression_eval',
+        verbose=verbose,
+        **eval_params)
+    return self.bq_utils.run_query(sql).to_dataframe()
+
+  def predict(self,
+              params: Dict[str, Union[str, float]],
+              overwrite_table: Optional[bool] = True,
+              verbose: Optional[bool] = False) -> None:
+    """Executes prediction job using BigQuery ML trained model.
+
+    Args:
+      params: Prediction parameters.
+      overwrite_table: If true, prediction results table is overwritten each
+        time prediction is made.
+      verbose: If set true, prints parsed SQL content.
+
+    Returns:
+      Prediction BigQuery job results as Pandas DataFrame.
+    """
+    predict_params = params
+    if overwrite_table:
+      predict_params.update({'overwrite_table': overwrite_table})
+
+    sql = utils.render_jinja_sql(
+        template_dir=_TEMPLATES_DIR,
+        template_name='regression_predict',
+        verbose=verbose,
+        **predict_params)
+    self.bq_utils.run_query(sql)
+    logging.info('Finished scoring.')
+    self._display_table_url(
+        table_path=params['output_table_path'], text='Prediction Table.')
+
+
+class PropensityModel(Model):
+  """Interacts with BigQuery ML to train, evaluate and predict."""
+
+  def train(self, verbose: bool = False) -> None:
+    """Trains propensity model in BigQuery ML.
+
+    Args:
+      verbose: If set true, prints parsed SQL content.
+
+    Returns:
+      Training BigQuery job results as Pandas DataFrame.
+    """
+    self._validate_training_params(model_type=ModelTypes.CLASSIFICATION)
+    train_params = self._get_train_params()
+    sql = utils.render_jinja_sql(
+        template_dir=_TEMPLATES_DIR,
+        template_name='classification',
+        verbose=verbose,
+        **train_params)
+    self.bq_utils.run_query(sql)
+    self._display_model_url()
 
   def evaluate(self,
                params: Optional[Mapping[str, Union[str, float]]] = None,
@@ -204,13 +365,13 @@ class PropensityModel:
     predict_params = params
     if overwrite_table:
       predict_params.update({'overwrite_table': overwrite_table})
+
     sql = utils.render_jinja_sql(
         template_dir=_TEMPLATES_DIR,
         template_name='prediction',
         verbose=verbose,
         **predict_params)
     self.bq_utils.run_query(sql)
-    project_id, dataset, table = str(params['output_table_path']).split('.')
-    url = f'&d={dataset}&p={project_id}&t={table}&page=table'
-    logging.info('Finished scoring. Table can be access via following URL:')
-    self._display_url(url, 'Scoring Table.')
+    logging.info('Finished scoring.')
+    self._display_table_url(
+        table_path=params['output_table_path'], text='Scoring Table.')
--- packages/utils/model_test.py
+++ packages/utils/model_test.py
@@ -12,7 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Tests for model."""
+
 import inspect
+from typing import List, Mapping, Union
 from unittest import mock
 
 from absl.testing import absltest
@@ -22,18 +24,38 @@ import pandas as pd
 from gps_building_blocks.cloud.utils import bigquery as bigquery_utils
 from compass.packages.utils import model
 
-_MODEL_PARAMS = {
-    'model_path': 'project.dataset.model',
-    'features_table_path': 'project.dataset.table',
-    'model_type': 'LOGISTIC_REG',
-    'data_split_method': 'AUTO_SPLIT',
-    'auto_class_weights': 'TRUE',
-    'feature_columns': ['feature1', 'feature2'],
-    'target_column': 'label'
-}
+
+def _get_model_params(
+    model_type: model.ModelTypes) -> Mapping[str, Union[str, List[str]]]:
+
+  if model_type == model.ModelTypes.REGRESSION:
+    custom_params = {
+        'model_type': 'LINEAR_REG',
+        'l2_reg': 0.0001,
+        'max_iterations': 50,
+        'learn_rate_strategy': 'LINE_SEARCH',
+        'early_stop': True,
+        'min_rel_progress': 0.001,
+        'enable_global_explain': True
+    }
+  elif model_type == model.ModelTypes.CLASSIFICATION:
+    custom_params = {
+        'model_type': 'AUTOML_CLASSIFIER',
+        'optimization_objective': 'MAXIMIZE_AU_ROC'
+    }
+  model_params = {
+      'model_path': 'project.dataset.model',
+      'features_table_path': 'project.dataset.table',
+      'data_split_method': 'AUTO_SPLIT',
+      'auto_class_weights': 'TRUE',
+      'feature_columns': ['feature1', 'feature2'],
+      'target_column': 'label'
+  }
+  model_params.update(custom_params)
+  return model_params
 
 
-class ModelTest(absltest.TestCase):
+class LTVModelTest(absltest.TestCase):
 
   def setUp(self):
     super().setUp()
@@ -42,33 +64,125 @@ class ModelTest(absltest.TestCase):
         bigquery_utils, 'BigQueryUtils', autospec=True).start()
     self.mock_display = mock.patch.object(
         display, 'display', autospec=True).start()
-    self.model = model.PropensityModel(self.mock_bigquery_utils, _MODEL_PARAMS)
+    self.model = model.LTVModel(self.mock_bigquery_utils,
+                                _get_model_params(model.ModelTypes.REGRESSION))
     self.mock_query = self.model.bq_utils.run_query
-    self.mock_df = self.mock_query.return_value.to_dataframe
 
-  def test_train_returns_job_result_as_dataframe(self):
-    train_sql_template = """
-      CREATE OR REPLACE MODEL
-        `{{ model_path }}`
-      OPTIONS(
-        MODEL_TYPE='{{ model_type }}',
-        INPUT_LABEL_COLS=['{{ target_column }}'],
-        AUTO_CLASS_WEIGHTS={{ auto_class_weights }}
-      ) AS
-      SELECT
-        {{ feature_columns |join(', ') }}, {{ target_column }}
-      FROM
-        `{{ table_path }}`;
-    """
-    expected_result = pd.DataFrame([{'iteration': 1, 'loss': 0.4}])
-    self.mock_df.return_value = expected_result
+  def test_train_calls_bq_api_with_train_query(self):
+    train_sql_template = inspect.cleandoc(
+        """CREATE OR REPLACE MODEL `{{ model_path }}` OPTIONS(
+            MODEL_TYPE='{{ model_type | upper }}',
+            L2_REG: {{ l2_reg }},
+            MAX_ITERATIONS: {{ max_iterations }},
+            LEARN_RATE_STRATEGY: '{{ learn_rate_strategy | upper }}',
+            EARLY_STOP: {{ early_stop | upper }},
+            MIN_REL_PROGRESS: {{ min_rel_progress }},
+            ENABLE_GLOBAL_EXPLAIN: {{ enable_global_explain | upper }}) AS
+            SELECT {{ feature_columns |join(', ') }}, {{ target_column }}
+            FROM `{{ features_table_path }}`;""")
+    expected_query = inspect.cleandoc(
+        """CREATE OR REPLACE MODEL `project.dataset.model` OPTIONS(
+            MODEL_TYPE='LINEAR_REG',
+            L2_REG: 0.0001,
+            MAX_ITERATIONS: 50,
+            LEARN_RATE_STRATEGY: 'LINE_SEARCH',
+            EARLY_STOP: TRUE,
+            MIN_REL_PROGRESS: 0.001,
+            ENABLE_GLOBAL_EXPLAIN: TRUE) AS
+            SELECT feature1, feature2, label
+            FROM `project.dataset.table`;""")
 
     with mock.patch('builtins.open',
                     mock.mock_open(read_data=train_sql_template)):
-      actual_result = self.model.train()
-    pd.testing.assert_frame_equal(actual_result, expected_result)
+      self.model.train()
+      self.mock_query.assert_called_once_with(expected_query)
 
-  def test_get_feature_info_returns_job_result_as_dataframe(self):
+  def test_evaluate_returns_eval_metrics_as_dataframe(self):
+    sql_template = inspect.cleandoc("""SELECT *
+            FROM ML.EVALUATE(MODEL `{{ model_path }}`);""")
+
+    params = {'eval_table_path': 'project.dataset.model'}
+    expected_metrics = pd.DataFrame([{
+        'mean_absolute_error': 0.01444,
+        'mean_squared_error': 0.0106,
+        'mean_squared_log_error': 0.0051,
+        'median_absolute_error': 0.0028,
+        'r2_score': 0.0344,
+        'explained_variance': 1.232
+    }])
+    expected_query = inspect.cleandoc("""SELECT *
+            FROM ML.EVALUATE(MODEL `project.dataset.model`);""")
+    self.mock_query.return_value.to_dataframe.return_value = expected_metrics
+
+    with mock.patch('builtins.open', mock.mock_open(read_data=sql_template)):
+      actual_metrics = self.model.evaluate(params=params)
+      pd.testing.assert_frame_equal(actual_metrics, expected_metrics)
+      self.mock_bigquery_utils.run_query.assert_called_once_with(expected_query)
+
+  def test_predict_calls_bq_api_with_prediction_query(self):
+    predict_sql_template = inspect.cleandoc(
+        """CREATE TABLE `{{ output_table_path }}` AS
+        SELECT *
+        FROM ML.PREDICT(MODEL `{{ model_path }}`,
+          (SELECT {{ feature_columns |join(', ') }}
+          FROM `{{ features_table_path }}`));""")
+    params = {
+        'model_path': 'project.dataset.model',
+        'features_table_path': 'project.dataset.features',
+        'output_table_path': 'project.dataset.scored',
+        'feature_columns': ['feature1', 'feature2'],
+        'overwrite_table': False
+    }
+    expected_query = inspect.cleandoc(
+        """CREATE TABLE `project.dataset.scored` AS
+        SELECT *
+        FROM ML.PREDICT(MODEL `project.dataset.model`,
+          (SELECT feature1, feature2
+          FROM `project.dataset.features`));""")
+
+    with mock.patch('builtins.open',
+                    mock.mock_open(read_data=predict_sql_template)):
+      self.model.predict(params=params)
+      self.mock_bigquery_utils.run_query.assert_called_once_with(expected_query)
+
+
+class PropensityModelTest(absltest.TestCase):
+
+  def setUp(self):
+    super().setUp()
+    self.addCleanup(mock.patch.stopall)
+    self.mock_bigquery_utils = mock.patch.object(
+        bigquery_utils, 'BigQueryUtils', autospec=True).start()
+    self.mock_display = mock.patch.object(
+        display, 'display', autospec=True).start()
+    self.model = model.PropensityModel(
+        self.mock_bigquery_utils,
+        _get_model_params(model.ModelTypes.CLASSIFICATION))
+    self.mock_query = self.model.bq_utils.run_query
+    self.mock_df = self.mock_query.return_value.to_dataframe
+
+  def test_train_calls_bq_api_with_train_query(self):
+    train_sql_template = inspect.cleandoc(
+        """CREATE OR REPLACE MODEL `{{ model_path }}` OPTIONS(
+            MODEL_TYPE='{{ model_type }}',
+            INPUT_LABEL_COLS=['{{ target_column }}'],
+            AUTO_CLASS_WEIGHTS={{ auto_class_weights }}) AS
+            SELECT {{ feature_columns |join(', ') }}, {{ target_column }}
+            FROM `{{ features_table_path }}`;""")
+    expected_query = inspect.cleandoc(
+        """CREATE OR REPLACE MODEL `project.dataset.model` OPTIONS(
+            MODEL_TYPE='AUTOML_CLASSIFIER',
+            INPUT_LABEL_COLS=['label'],
+            AUTO_CLASS_WEIGHTS=TRUE) AS
+            SELECT feature1, feature2, label
+            FROM `project.dataset.table`;""")
+
+    with mock.patch('builtins.open',
+                    mock.mock_open(read_data=train_sql_template)):
+      self.model.train()
+      self.mock_query.assert_called_once_with(expected_query)
+
+  def test_get_feature_info_returns_features_info_as_dataframe(self):
     feature_info_sql_template = """
       SELECT
         *
@@ -91,7 +205,7 @@ class ModelTest(absltest.TestCase):
       actual_result = self.model.get_feature_info()
     pd.testing.assert_frame_equal(actual_result, expected_result)
 
-  def test_evaluate_returns_job_list_of_eval_metrics_dataframes(self):
+  def test_evaluate_returns_eval_metrics_as_list_of_dataframes(self):
     sql_template = """
       SELECT
         *
@@ -143,11 +257,11 @@ class ModelTest(absltest.TestCase):
 
     with mock.patch('builtins.open', mock.mock_open(read_data=sql_template)):
       actual_dataframes = self.model.evaluate(params=params)
+      self.assertEqual(len(actual_dataframes), len(expected_dataframes))
       for actual, expected in zip(actual_dataframes, expected_dataframes):
         pd.testing.assert_frame_equal(actual, expected)
-    self.assertEqual(len(actual_dataframes), len(expected_dataframes))
 
-  def test_predict_returns_returns_empty_row_iterator(self):
+  def test_predict_calls_bq_api_with_prediction_query(self):
     predict_sql_template = inspect.cleandoc(
         """CREATE TABLE `{{ output_table_path }}` AS
         SELECT *
